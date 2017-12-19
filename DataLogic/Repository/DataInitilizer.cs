5875c94620247c7b1f54a83e038174d1eb3fb87d
Christoffer Elmesiöö <c.elmesioo@gmail.cm>
2017-12-19 20:50:46 +0100

    DbInitilizer&UserProp's

---
+++ DataLogic/Repository/DataInitilizer.cs
@@ -0,0 +1,14 @@
+﻿using DataLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DataLogic.Repository
+{
+    public class DataInitilizer : DropCreateDatabaseIfModelChanges<ApplicationDbContext>
+    {
+
+    }
+}
